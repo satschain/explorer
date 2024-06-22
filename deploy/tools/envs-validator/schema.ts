188904852ccbbfde1d3e57fb8d26419f4df171d0
Piyush Aggarwal <just_keep_debugging@192.168.1.5>
2024-06-22 21:32:17 +0530

    fix: schema validation

--- deploy/tools/envs-validator/schema.ts
+++ deploy/tools/envs-validator/schema.ts
@@ -348,6 +348,7 @@ const navItemExternalSchema: yup.ObjectSchema<NavItemExternal> = yup
   .object({
     text: yup.string().required(),
     url: yup.string().test(urlTest).required(),
+    isActive: yup.mixed().optional(), // Adjusted to include isActive, allowing any type
   });
 
 const footerLinkSchema: yup.ObjectSchema<CustomLink> = yup
