05a2873c0d1df773d8c5afd4703c088aabecd99a
Piyush Aggarwal <just_keep_debugging@192.168.1.5>
2024-06-24 12:05:40 +0530

    basic sanoty

--- stubs/addressParams.ts
+++ stubs/addressParams.ts
@@ -1,6 +1,6 @@
 import type { AddressParam } from 'types/api/addressParams';
 
-export const ADDRESS_HASH = '0x2B51Ae4412F79c3c1cB12AA40Ea4ECEb4e80511a';
+export const ADDRESS_HASH = 'ac1q2gu35g5c9752r4yzvntfau763gcv6pff000000';
 
 export const ADDRESS_PARAMS: AddressParam = {
   hash: ADDRESS_HASH,
